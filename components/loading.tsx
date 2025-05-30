46d220564a9fe2c4fde70f4bd156e3d9a2e60170
harshdeep61034 <hdeep61034@gmail.com>
2025-05-31 03:57:17 +0530

    tweaks

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,9 +1,10 @@
+import { LoaderCircle, LoaderPinwheel } from "lucide-react";
 import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className=" w-24 h-24 border-16 border-solid border-black border-t-4 border-t-gray-500 rounded-full animate-spin"></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black">
+      <LoaderCircle className="w-14 h-14 text-white animate-[spin_1s_linear_infinite]" />
     </div>
   );
 };
